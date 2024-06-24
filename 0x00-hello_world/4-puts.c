bbda687db497bfd9548560df1b01aaf26d56ce96
rutendomususa <ruemususa6700@gmail.com>
2024-06-24 05:55:26 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return - must return always return a zero
+ */
 
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
-
